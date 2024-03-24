a04a7e2af801d00533b1e87ab2697c4de003ecb1
ingmapache <mapache.rgp@gmail.com>
2024-03-24 14:11:06 -0600

    FIX Palindrome check loop DO result div code
    Fixed the logic behind the palindromChecker function, also added an
    event listener for enter key, so user can get results without clicking
    the button.
    
    Added code so that user can know if its actually a palindrome or not.

--- main-script.js
+++ main-script.js
@@ -1,30 +1,40 @@
 const textInput = document.getElementById('text-input');
 const checkBtn = document.getElementById('check-btn');
-const results = document.getElementById('results');
+const results = document.getElementById('result');
 
 let normalizedStringArray;
-let isPalindrome = true;
+
 
 const palindromeChecker = () => {
     if (textInput.value === "") {
         alert('Please input a value');
     } else {
         stringNormalizer();
+        let isPalindrome = true;
         for (let i = 0; i < normalizedStringArray.length / 2; i++) {
-            if (normalizedStringArray[i] !== normalizedStringArray[normalizedStringArray.length - 1 -i]) {
+            if (normalizedStringArray[i] !== normalizedStringArray[normalizedStringArray.length - 1 - i ]) {
                 isPalindrome = false;
                 break;
             }
         }
-        console.log(normalizedStringArray);
-        console.log(isPalindrome);
+        if (isPalindrome) {
+        results.innerHTML = `<p class="resultText"><span class="bold">${textInput.value}</span> is a palindrome.</p>`;
+        } else {
+         results.innerHTML = `<p class="resultText"><span class="bold">${textInput.value}</span> is not a palindrome.</p>`;
+        }
+        textInput.value = "";
     }
 
 
 };
 
 const stringNormalizer = () => {
-    normalizedStringArray = textInput.value.toString().toLowerCase().replace(/\W/g, '').split('');
+   normalizedStringArray = textInput.value.toString().toLowerCase().replace(/[\W_]+/g, '').split('');
 }
 
 checkBtn.addEventListener('click', palindromeChecker);
+textInput.addEventListener('keydown', function (e) {
+    if (e.key === 'Enter') {
+      palindromeChecker();
+    }
+});
